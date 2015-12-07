6b4fabbf029aa8fa2510850175a0f466200a5426
Wladimir <Wladimir.Zholobenko@hhu.de>
2015-12-07 23:27:10 +0100

    done

--- euler_osc.cxx
+++ euler_osc.cxx
@@ -11,16 +11,20 @@ int main() {
     xb[0] = 1; xb[1] = 0;
     
     const double dt = M_PI/10;
-    const double t_end = 20*M_PI;
+    const double t_end = 5*M_PI;
     double t = 0;
+    
+    double f_t, b_t;
     
     ofstream out("d");
     for (t; t<=t_end; t+=dt) {
       out << t << "\t" << xf[0] << "\t" << xb[0] << endl;
+      f_t = xf[0];
       xf[0] = xf[0]+dt*xf[1];
-      xf[1] = xf[1]-dt*xf[0];
+      xf[1] = xf[1]-dt*f_t;
+      b_t = xb[0];
       xb[0] = (xb[0]+dt*xb[1])/(1+dt*dt);
-      xb[1] = (-dt*xb[0]+xb[1])/(1+dt*dt);
+      xb[1] = (-dt*b_t+xb[1])/(1+dt*dt);
     }
     out << t << "\t" << xf[0] << "\t" << xb[0] << endl;
     out.close();
